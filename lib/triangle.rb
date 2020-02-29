8610ed62ff2185e26361f09b18d4b7a85b1a8bc6
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-29 17:14:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
-  # write code here
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+  
+  def kind 
+  end
+  
 end
