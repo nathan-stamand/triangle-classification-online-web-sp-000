20195b571a708f91b4daecfe2fae897d4c835a3a
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-29 18:19:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
   
   def kind 
 
-    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one == nil || @side_two == nil || @side_three == nil || @side_one + @side_two < @side_three || @side_one + @side_three < @side_two || @side_two + @side_three < @side_one
+    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one == nil || @side_two == nil || @side_three == nil || @side_one + @side_two <= @side_three || @side_one + @side_three <= @side_two || @side_two + @side_three <= @side_one
       begin
         raise TriangleError
       end
