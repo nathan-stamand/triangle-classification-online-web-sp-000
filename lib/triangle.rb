8fe645e6c4623b129cc9369d7db4f49417de33b4
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-29 18:18:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,9 +16,6 @@ class Triangle
     if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one == nil || @side_two == nil || @side_three == nil || @side_one + @side_two < @side_three || @side_one + @side_three < @side_two || @side_two + @side_three < @side_one
       begin
         raise TriangleError
-      rescue TriangleError => error 
-        # puts error.message
-        return error.message
       end
     end
     
