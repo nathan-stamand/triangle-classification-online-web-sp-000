22912b7cf1782cc352c92e070b89bc2a5d7e6490
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-29 17:59:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,35 +1,40 @@
 class Triangle
+  attr_accessor :side_one, :side_two, :side_three
   
   def initialize(side_one, side_two, side_three)
+
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
+
     if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
       begin
         raise TriangleError 
-      rescue TriangleError => error 
-        error.message
+      rescue TriangleError 
+        puts TriangleError.message
       end
+    end
+  
   end
   
   def kind 
     
-    
-    elsif @side_one == @side_two && @side_one == @side_three 
+    if @side_one == @side_two && @side_one == @side_three && @side_two == @side_three
       :equilateral 
     elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
       :isosceles 
     else 
       :scalene
     end 
+
   end
   
   class TriangleError < StandardError 
     
-    def message 
+    def self.message 
       "You done fucked up this triangle, dawg. It's gonna be a no for me."
     end
     
   end 
-  
+
 end
