b6a59718720b2db4a58589663a41c63c5e63bf70
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-29 17:31:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
   
   def kind 
-    if !@side_one || !@side_two || !@side_three
+    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
       begin
       raise TriangleError 
       rescue TriangleError => error 
